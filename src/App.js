94c3e1cfa7c0bd602139996d4bf73adcdae7582c
emarose <emanuelamerio.roseu@gmail.com>
2021-10-10 21:08:45 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Navigation from './Components/Navigation/Navigation';
+import Home from './Components/Home/Home';
+import Footer from './Components/Footer/Footer';
+import Contacto from './Components/Contacto/Contacto';
+import Promociones from './Components/Promociones/Promociones'
+
+
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Switch>
+        <Route path="/contacto"><Navigation/><Contacto/><Footer/></Route>
+        <Route path="/promociones"><Navigation/><Promociones/><Footer/></Route>
+        <Route path="/" exact><Home /></Route>
+
+      </Switch>
+    </Router>
+    </>
+  );
+}
+
+export default App;
